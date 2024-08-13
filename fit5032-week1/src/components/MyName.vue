26dde1837f01f7c060a354085fef16a09618fd57
Suzyy0 <140936897+Suzyy0@users.noreply.github.com>
2024-08-13 12:44:19 +1000

    Commit lab activities

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="my-name">
+      <p>- Name : Suji Namgung</p>
+      <p>- Github Address : <a href= "https://github.com/Suzyy0" target="_blank" rel="noopener">https://github.com/Suzyy0</a></p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MyName'
+  }
+  </script>
+  
+  <style scoped>
+  .my-name {
+    font-size: 20px;
+    color: #fff
+  }
+  </style>
