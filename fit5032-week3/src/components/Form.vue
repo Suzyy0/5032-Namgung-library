66eee81797e8a2b2c682c4fbd5ec9a0662b0fda9
Suzyy0 <140936897+Suzyy0@users.noreply.github.com>
2024-08-13 22:42:16 +1000

    Finalize the week4 activity

--- fit5032-week3/src/components/Form.vue
+++ fit5032-week3/src/components/Form.vue
@@ -1,30 +1,3 @@
-<!-- <template>
-    <div>
-    <h1>User Information Form / Credentials</h1>
-        <form>
-            <label for="username">Username:</label><br>
-            <input type="text" id="username" name="username"><br>
-            
-            <label for="password">Password:</label><br>
-            <input type="password" id="password" name="password"><br>
-            
-            <label for="isAustralian">Australian Resident?</label><br>
-            <input type="checkbox" id="isAustralian" name="isAustralian"><br>
-            
-            <label for="reason">Reason For Joining:</label><br>
-            <textarea id="reason" name="reason" rows="3"></textarea><br>
-            
-            <label for="gender">Gender</label><br>
-            <select id="gender">
-                <option value="female">Female</option>
-                <option value="male">Male</option>
-                <option value="other">Other</option>
-            </select>
-        </form>
-    </div>
-</template> -->
-
-
 <template>
     <div class="container mt-5"> 
         <div class="row">
@@ -34,11 +7,19 @@
                     <div class="row mb-3">
                         <div class="col-md-6">
                             <label for="username" class="form-label">Username </label>
-                            <input type="text" class="form-control" id="username" v-model="formData.username">
+                            <input type="text" class="form-control" id="username" 
+                            @blur="() => validateName(true)"
+                            @input="() => validateName(false)"
+                            v-model="formData.username" />
+                            <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
                         </div> 
                         <div class="col-md-6">
                             <label for="password" class="form-label">Password</label> 
-                            <input type="password" class="form-control" id="password" v-model="formData.password">
+                            <input type="password" class="form-control" id="password" 
+                            @blur="() => validatePassword(true)"
+                            @input="() => validatePassword(false)"
+                            v-model="formData.password" />
+                            <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
                         </div> 
                     </div>
                     <div class="row mb-3">
@@ -52,16 +33,24 @@
                         </div> 
                         <div class="col-md-6">
                             <label for="gender" class="form-label">Gender</label> 
-                            <select class="form-select" id="gender" v-model="formData.gender">
+                            <select class="form-select" id="gender"
+                            v-model="formData.gender"
+                            @change="validateGender">
                                 <option value="male">Male</option> 
                                 <option value="female">Female</option> 
                                 <option value="other">Other</option>
-                            </select> 
+                            </select>
+                            <div v-if="errors.gender" class="text-danger">{{ errors.gender }}</div> 
                         </div>
                     </div> 
                     <div class="mb-3">
                         <label for="reason" class="form-label">Reason for joining</label> 
-                        <textarea class="form-control" id="reason" rows="3" v-model="formData.reason"></textarea>
+                        <textarea class="form-control" id="reason" rows="3" 
+                            @blur="() => validateName(true)"
+                            @input="() => validateName(false)"
+                            v-model="formData.reason">
+                        </textarea>
+                        <div v-if="errors.reason" class="text-danger">{{ errors.reason }}</div>
                     </div> 
                     <div class="text-center">
                         <button type="submit" class="btn btn-primary me-2">Submit</button> 
@@ -92,23 +81,101 @@
 
 
 <script setup>
-    import { ref } from 'vue';
-    
-    const formData = ref({
+import { ref } from 'vue';
+// import { errorMessages } from 'vue/compiler-sfc';
+
+const formData = ref({
+    username: '',
+    password: '',
+    isAustralian: false,
+    reason: '',
+    gender: ''
+});
+
+const submittedCards = ref([]);
+
+const submitForm = () => {
+    validateName(true);
+    validatePassword(true);
+    validateGender(true);
+    validateReason(true);
+    if (!errors.value.username && 
+    !errors.value.password && 
+    !errors.value.gender && 
+    !errors.value.reason) {
+        submittedCards.value.push({
+            ...formData.value });
+        clearForm();
+    }
+};
+
+const clearForm = () => {
+    formData.value = {
         username: '',
         password: '',
         isAustralian: false,
         reason: '',
         gender: ''
-    });
-    
-    const submittedCards = ref([]);
-
-    const submitForm = () => {
-        submittedCards.value.push({
-            ...formData.value
-        });
     };
+};
+
+const errors = ref({
+    username: null,
+    password: null,
+    resident: null,
+    reason: null,
+    gender: null,
+});
+
+const validateName = (blur) => {
+    if (formData.value.username.length < 3) {
+        if (blur) errors.value.username = "Name must be at least 3 characters.";
+    } else {
+        errors.value.username = null;
+    }
+};
+
+const validatePassword = (blur) => {
+    const password = formData.value.password;
+    const minLength = 8;
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    if (password.length < minLength) {
+        if (blur) errors.value.password = `Password must be at least ${minLength} charaters long.`;
+    } else if (!hasUppercase) {
+        if (blur) errors.value.password = "Password must contain at least one uppercase letter.";
+    } else if (!hasLowercase) {
+        if (blur) errors.value.password = "Password must contain at least one lowercase letter.";
+    } else if (!hasNumber) {
+        if (blur) errors.value.password = "Password must contain at least one number.";
+    } else if (!hasSpecialChar) {
+        if (blur) errors.value.password = "Password must contain at least one special character.";
+    } else {
+        errors.value.username = null;
+    }
+};
+
+// Q) WHY 'blur' is not used for the gender dropdown?
+// A) Because selection is immediate and definitive, eliminating the need for validation upon losing focus!
+const validateGender = () => {
+    if (!formData.value.gender) {
+        errors.value.gender = "Please select a gender.";
+    } else {
+        errors.value.gender = null;
+    }
+};
+
+const validateReason = (blur) => {
+    if (formData.value.reason.length < 3) {
+        if (blur) errors.value.reason = "Reason must be at least 10 characters.";
+    } else {
+        errors.value.reason = null;
+    }
+};
+
 </script>
 
 
