9754cbe5fa848f1805e80450c556d31c36969d13
Suzyy0 <140936897+Suzyy0@users.noreply.github.com>
2024-08-20 19:59:01 +1000

    Week5.5 Adding a Router to navigate pages

--- namgung-library_5/src/router/index.js
+++ namgung-library_5/src/router/index.js
@@ -1,16 +1,23 @@
-// // router/index.js
-// import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
 
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'home',
-//       component: HomeView
-//     },
-//   ]
-// })
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutView
+  }
+]
 
-// export default router
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
